feced67325def715905973d620d690e30bae7665
Duncan Warutere <duncanwarutere79@gmail.com>
2023-04-17 16:20:46 +0300

    update place_order.php

--- function/place_order.php
+++ function/place_order.php
@@ -14,18 +14,21 @@
 
 		$t_id = $r_id;
 		$date = date("M d, Y");
-		
-		// insert transaction record
-		$query = mysqli_query($conn, "INSERT INTO `transaction` (transaction_id, customerid, amount, order_status, order_date) VALUES ('$t_id', '$cid', '$total', 'ON HOLD', '$date')") or die(mysqli_error($conn));				
 
 		// insert transaction details
 		$p_id = $_POST['pid'];
 		$oqty = $_POST['qty'];
+		
+					
+
+		
 
 		foreach($p_id as $i => $pro_id) {
 			// check if product exists
 			$check_product = mysqli_query($conn, "SELECT * FROM `product` WHERE `product_id`='$pro_id'");
 			if(mysqli_num_rows($check_product) > 0) {
+				// insert transaction record
+				$query = mysqli_query($conn, "INSERT INTO `transaction` (transaction_id, customerid, product_id, amount, order_status, order_date) VALUES ('$t_id', '$cid','$pro_id', '$total', 'ON HOLD', '$date')") or die(mysqli_error($conn));	
 				// product exists, insert transaction detail
 				mysqli_query($conn, "INSERT INTO `transaction_detail` (`product_id`, `order_qty`, `transaction_id`) VALUES ('$pro_id', '$oqty[$i]', '$t_id')") or die("Error: Unable to insert transaction detail - ".mysqli_error($conn));
 			} else {
